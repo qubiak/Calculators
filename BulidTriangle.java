5c5400415c531ad3cb8515bf39aae98a6ad5d8f3
qubiak <53450320+qubiak@users.noreply.github.com>
2019-10-09 11:57:59 +0200

    Add files via upload

---
+++ BulidTriangle.java
@@ -0,0 +1,41 @@
+package bulidtriangle;
+
+import java.util.Scanner;
+
+/**
+ * @author qubiak
+ *
+ */
+public class BulidTriangle {
+
+    public static void main(String[] args) {
+
+        double a;
+        double b;
+        double c;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Let's see if we can bulid a triangle");
+        System.out.println("Give my a first side");
+        System.out.println("a= ");
+        a = in.nextDouble();
+        System.out.println("Give my a second side");
+        System.out.println("b= ");
+        b = in.nextDouble();
+        System.out.println("Give my a third side");
+        System.out.println("c= ");
+        c = in.nextDouble();
+
+        if (a + b > c && a + c > b && b + c > a) {
+            System.out.println("We can bulid a triangle");
+            System.out.print("The perimeter of this trangile will be ");
+            System.out.println(a + b + c);
+        } 
+        else {
+            System.out.println("Can't bulid a triangile");
+        }
+
+    }
+
+}
